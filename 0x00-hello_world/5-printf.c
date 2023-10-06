33202497ca2709954f3f340f31ffa633fe840d0c
benjie1331 <benemedhat@gmail.com>
2023-10-06 06:00:45 -0700

    up

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main-entry point
- *
- * return: 0 success
-*/
+/**
+ * main - printf fcn to print
+ * Return: 0 success
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
