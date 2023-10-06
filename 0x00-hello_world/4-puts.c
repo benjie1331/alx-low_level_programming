fa32d6957e92f837a04f7aac5e06c55840d1df61
benjie1331 <benemedhat@gmail.com>
2023-10-06 04:44:31 -0700

    up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*
- *main function that takes no arguments 
- */
+*main function that takes no arguments 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
