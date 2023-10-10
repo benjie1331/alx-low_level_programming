8f1eac5b54148e1eee36d94b4f01e308cabaf7d0
benjie1331 <benemedhat@gmail.com>
2023-10-10 14:19:24 -0700

    up

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /*
  * _islower - function that checks if char is lowerchase
+ * Description: function that prints lowercase only
  * Return: 1 if lower, 0 if not
  */
 int _islower(int c)
