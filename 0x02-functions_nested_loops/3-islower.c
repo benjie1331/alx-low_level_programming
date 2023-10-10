f3735e826a111d953cfd9afcfd70a239e9373dd5
benjie1331 <benemedhat@gmail.com>
2023-10-10 14:24:40 -0700

    up

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - function that prints lowercase
  * @c: input char
- * Description: function using _putchar function to print lowercase
+ * Description: function to print lowercase
  * Return: 1 if  lowercase, 0 if not
  */
 int _islower(int c)
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,3 @@
+#include "main.h"
+/* _isalpha - function that checks for alphabet
+ * Description
