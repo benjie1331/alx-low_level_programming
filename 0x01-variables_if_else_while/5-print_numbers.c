623c29559bbc666debfb54751e1072a9236fd4b3
benjie1331 <benemedhat@gmail.com>
2023-10-11 13:57:57 -0700

    up

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <sdtio.h>
+/**
+ *  main - entry point
+ *  Description: prints base 10 single num
+ *  Return: 0 success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
