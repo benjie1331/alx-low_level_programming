245ca2b315897db26e1e7b426f79a294d45a4b87
benjie1331 <benemedhat@gmail.com>
2023-10-11 11:32:20 -0700

    up

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
  * add - fcn to add
- * Description: fcn to add
+ * @x: input
+ * @y: input
+ * Description: Function that adds 2 integrs
+ * Return: Returns the sum of the 2 integers
  */
 
 int add(int x, int y)
