a60bc18b0f74b0c7dd2992c6bd6557cc0b1ee38b
benjie1331 <benemedhat@gmail.com>
2023-10-11 12:13:11 -0700

    up

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 	{
 		printf("%d, ", counter);
 	}
+	}
 	else
 	{
 		for (counter = n; counter < 98; counter++)
 		{	printf("%d, ", counter);
 	
 		}
-	}
 	}
 	printf("98\n");
 }
