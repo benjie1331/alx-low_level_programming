a1aae2fee4c88c8344b28215fc4ae8ad7efdafc2
benjie1331 <benemedhat@gmail.com>
2023-10-11 10:34:28 -0700

    up

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Function that prints the timetables
+ * Description: Function that prints the time tables
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 10; j++)
+		{
+		k = i * j;
+		if (j > 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		if (k <= 9)
+		{
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else
+		{	_putchar((k / 10) + 48);
+			_putchar((k % 10) + 48);
+		}
+		}
+		_putchar('\n');
+	}
+}
