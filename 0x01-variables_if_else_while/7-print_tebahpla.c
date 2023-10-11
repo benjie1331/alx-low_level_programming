b9147bee75fe49af9902ce8346fb443d44964136
benjie1331 <benemedhat@gmail.com>
2023-10-11 14:12:22 -0700

    up

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  Description: prints lowercase alphabet in reverse
+ *  Return: 0 success
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	return (0);
+}
