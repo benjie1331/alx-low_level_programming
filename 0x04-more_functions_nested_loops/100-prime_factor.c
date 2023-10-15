73ff80e6050811c30050f8c8910474bac335a7ba
benjie1331 <benemedhat@gmail.com>
2023-10-15 11:49:17 -0700

    up

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * Description: prints the largest prime factor 
+ * Return: 0 success 
+ */
+int main (void)
+{
+
+	long n;
+
+	long j;
+
+	long largest_factor; 
+
+	j = 2;
+
+	n = 612852475143;
+
+	for ( j = 2; j < 612852475143; j++)
+	{
+		 
+		}
+
+		else if ( j % 2 == 0 || j % 3 == 0)
+		{
+			continue;
+		}
+		else 
+		{
+
+		}
+
+	}
+		
+
+
+			return (0);
+}
