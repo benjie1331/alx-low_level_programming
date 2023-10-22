2ef5e2f2a602cd9c0e39fbd5c042a30a70923bf4
benjie1331 <benemedhat@gmail.com>
2023-10-22 13:52:30 -0700

    up

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 
 	if (i % 2 == 0)
 	{
-		for (j = i / 2; j <= i; j++)
+		for (j = i / 2; j < i; j++)
 		{
 			_putchar(str[j]);
 		} _putchar('\n');
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	{
 		i--;
 
-		for (j = i / 2; j <= i; j++)
+		for (j = i / 2; j < i; j++)
 		{
 			_putchar(str[j]);
 		} _putchar('\n');
