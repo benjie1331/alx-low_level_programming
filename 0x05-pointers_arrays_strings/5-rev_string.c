65d1dd6eaea3827cf6cd39afde95dca99e4f5b5f
benjie1331 <benemedhat@gmail.com>
2023-10-22 13:00:10 -0700

    up

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - fcn to reverse a string
+ * @s: input
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	int j = 0;
+
+	char temp;
+
+	while (s[i] != 0)
+	i++;
+	i--;
+
+	while (j < i)
+	{
+		 temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+
+		i--;
+		j++;
+
+	}
+}
