fc45ff2a3b645c054c6e9a3fd5f8123fd84de2b9
benjie1331 <benemedhat@gmail.com>
2023-10-22 18:46:39 -0700

    up

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - fcn that copies the string
+ * @dest: input
+ * @src: input
+ * Return: char pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *og = dest;
+
+	while (*src != 0)
+	{
+		*dest = *src;
+
+		dest++;
+
+		src--;
+	}
+
+	return (og);
+}
