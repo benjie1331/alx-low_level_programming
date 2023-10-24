b6603acfa58959be98621c48d1cade6f647fde47
benjie1331 <benemedhat@gmail.com>
2023-10-23 21:04:30 -0700

    up

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - fcn that gets the length of a prefix
+ * @s: input
+ * @accept: input
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, m, k, counter;
+
+	counter = 0;
+
+	i = 0;
+
+	j = 0;
+
+	while (accept[i] != 0)
+		i++;
+	while (s[j] != 0)
+		j++;
+
+	for (k = 0; k <= j; k++)
+	{
+		for (m = 0; m <= i; m++)
+		{
+			if (s[k] == accept[m])
+				counter++;
+		}
+	} return counter;
+}
