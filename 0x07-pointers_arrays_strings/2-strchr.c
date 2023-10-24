711eb6ee9569d0fd18ee1d11f5823bf1d889b2a2
benjie1331 <benemedhat@gmail.com>
2023-10-23 20:34:05 -0700

    up

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
- * *_strchr - fcn that located a char in string
+ * *_strchr - fcn that locates char in string
  * @s: input
  * @c: input
  * Return: pointer to char
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
+	int i;
 
-int j;
-
-while (s[i] != 0)
-i++;
-for (j = 0; j < i; j++)
-{
-if (s[j] == c)
-{
-return (&s[j]);
-}
-} return (NULL);
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
 }
