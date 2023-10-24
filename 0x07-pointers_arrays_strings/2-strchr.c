26a40142924c089227019bd39757e3a1e008a29a
benjie1331 <benemedhat@gmail.com>
2023-10-23 18:35:17 -0700

    up

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strchr - fcn that located a char in string
+ * @s: input
+ * @c: input
+ * Return: pointer to char
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	int j;
+
+	while (s[i] != 0)
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == c)
+		{
+			return (&s[j]);
+			break;
+		}
+	} return (NULL);
+}
