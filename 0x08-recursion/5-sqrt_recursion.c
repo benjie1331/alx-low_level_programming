2bbddd3d8004764e5657f127e8d8e19bdf13cae5
benjie1331 <benemedhat@gmail.com>
2023-10-25 02:51:13 -0700

    up

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - fcn that returns the sqrt of a number
+ * @n: input
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int k;
+
+	if ((n ^ 0.5) % 1 == 0)
+	{
+		k = (n ^ 0.5);
+	return (k);
+	}
+	else
+		return (-1);
+
+}
