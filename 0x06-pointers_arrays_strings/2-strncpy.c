f0ecb4c77f65c300cb9bf20edac5762cb8627861
benjie1331 <benemedhat@gmail.com>
2023-10-31 04:29:34 -0700

    up

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strncpy - fcn that copies string 
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: pointer to char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
