311c06a98fb9bfb94a89f5233442ee8085516475
benjie1331 <benemedhat@gmail.com>
2023-11-04 10:55:50 -0700

    up

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input
+ * @arv: input
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+
+	return (0);
+}
