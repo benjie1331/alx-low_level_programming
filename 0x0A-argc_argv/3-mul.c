9cbc84b151ca3b8244dbd718d3b5f01a3f5398b2
benjie1331 <benemedhat@gmail.com>
2023-11-06 05:21:00 -0800

    up

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - entry point
  * @argc: input
@@ -7,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
