812d17684dd4b222784382f6e93ec171b4291a1b
benjie1331 <benemedhat@gmail.com>
2023-11-06 04:10:13 -0800

    up

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc:input
+ * @argv:input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc - 1);
+	return (0);
+}
