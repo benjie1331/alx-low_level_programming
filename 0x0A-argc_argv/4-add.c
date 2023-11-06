06b56164974204fca4463e6fac93a68dc9553251
benjie1331 <benemedhat@gmail.com>
2023-11-06 08:33:25 -0800

    up

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc:input
+ * @argv:input
+ * Return: input
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+				return (printf("Error"), 1);
+		sum += atoi(argv[argc]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
