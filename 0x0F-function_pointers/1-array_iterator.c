bab79aaaa7eaee1ea8ca7f39a63b27f6402b4747
benjie1331 <benemedhat@gmail.com>
2023-11-08 14:39:30 -0800

    up

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int last_elment = &array + size - 1;
+	int *last_elment = array + size - 1;
 
 	if (array && size && action)
 	{
