1bed425c046e7680f5facba6ac89aecf50c35c3f
benjie1331 <benemedhat@gmail.com>
2023-11-08 14:29:05 -0800

    up

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointer.h"
+/**
+ * array_iterator - fcn that executes a fcn given as a para
+ * @array:input
+ * @size: size of arr
+ * @action: fcn
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int last_elment = &array + size - 1;
+
+	if (array && size && action)
+	{
+		while (array <= last_elment)
+			action(*array++);
+	}
+}
