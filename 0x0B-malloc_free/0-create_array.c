ff18e257130372e3fc6e851af67cd62790a8b0dd
benjie1331 <benemedhat@gmail.com>
2023-11-13 16:43:02 -0800

    up

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * *create_array - fcn that creates arrays
  * @size: input
@@ -9,15 +10,13 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 
-	int i;
-
-	i == 0;
+	unsigned int i;
 
 	ptr = malloc(size);
 
 	if (size == 0)
 		return (0);
-	for (i; i <= size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		ptr[i] = c;
 	}
