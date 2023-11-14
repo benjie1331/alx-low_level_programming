166bee0e051afa49294f7a04c89723c8bc23c999
benjie1331 <benemedhat@gmail.com>
2023-11-13 17:07:56 -0800

    up

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - fcn that retuenrs ptr
+ * @str: ptr
+ * Return: ptr
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	int i, size;
+
+	size = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size] != '/0')
+		size++;
+
+	ptr = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{	return (NULL);
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = str[i];
+	}
+
+	}
+
+	return (ptr);
+}
