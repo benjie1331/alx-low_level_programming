f3dd9b7e24095708feb12a1c2f02ed87f1242d04
benjie1331 <benemedhat@gmail.com>
2023-11-18 15:57:19 -0800

    up

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - fcn that returns a poniter to 2d arrasy
+ * @width: input
+ * @height: input
+ * Return: pointer to pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr = malloc(height * sizeof(int *));
+
+	int i, j, k;
+
+	if (width <= 0 || height <= 0 || ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (k = 0; k < height; k++)
+		{
+			ptr[k] = malloc(width * sizeof(int));
+		}
+
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				ptr[i][j] = 0;
+			}
+		}
+	}
+	return (ptr);
+}
